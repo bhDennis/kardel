3f501de43c55ba41a297c2510e593311275d0000
dennis <dennis.he@aihuishou.com>
2018-03-10 16:12:42 +0800

    test

--- kardel-web/src/test/java/me/dennis/exercise/test/CommonUtilsTest.java
+++ kardel-web/src/test/java/me/dennis/exercise/test/CommonUtilsTest.java
@@ -3,6 +3,8 @@ package me.dennis.exercise.test;
 import me.dto.Bean;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.xml.DefaultBeanDefinitionDocumentReader;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import utils.CommonUtils;
@@ -22,9 +24,16 @@ public class CommonUtilsTest {
 
 //      System.out.println(Bean.class.getConstructor(String.class,List.class)); // 如果将Bean的构造器定义为private，此处会抛出异常 java.lang.NoSuchMethodException: other.Bean.<init>(java.lang.String, java.util.List)
 
-        Constructor<Bean> constructor = CommonUtils.accessibleConstructor(Bean.class,String.class,List.class); // accessibleConstructor作用是使Bean的构造器即使为private也让它变得可用
-        Bean bean = constructor.newInstance("hzx",new ArrayList<>());
+//        Constructor<Bean> constructor = CommonUtils.accessibleConstructor(Bean.class,String.class,List.class); // accessibleConstructor作用是使Bean的构造器即使为private也让它变得可用
+//        Bean bean = constructor.newInstance("hzx",new ArrayList<>());
+//
+//        System.out.println("constructor:"+constructor+",bean:"+bean);
 
-        System.out.println("constructor:"+constructor+",bean:"+bean);
+          Class<?> documentReaderClass = A.class;
+
+          A a = (A)BeanUtils.instantiateClass(documentReaderClass);
+          C c = C.class.cast(a);
+          System.out.println(a);
+          System.out.println(c);
     }
 }
