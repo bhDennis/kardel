e79485d3daac73a7dabdcff7c25dafe79652e9f2
dennis <dennis@dennisdeMacBook-Pro.local>
2018-04-10 23:38:22 +0800

    解释器模式

---
+++ kardel-web/src/main/java/mode/chapter27/AbstractExpression.java
@@ -0,0 +1,9 @@
+package mode.chapter27;
+
+/**
+ * Created by dennis on 2018/4/10.
+ */
+public abstract class AbstractExpression {
+
+    abstract void interpret(Context context);
+}
---
+++ kardel-web/src/main/java/mode/chapter27/NoterminalExpression.java
@@ -0,0 +1,15 @@
+package mode.chapter27;
+
+/**
+ * 非终结符表达式：为文法中的非终结符实现解释操作
+ *
+ * Created by dennis on 2018/4/10.
+ */
+public class NoterminalExpression  extends AbstractExpression{
+
+    @Override
+    void interpret(Context context) {
+
+        System.out.println("非端解释器");
+    }
+}
