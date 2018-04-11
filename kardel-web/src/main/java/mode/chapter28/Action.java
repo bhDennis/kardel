1df0590552b01b14f0e2d8c5a7bb63e67c3b0cc7
dennis <dennis@dennisdeMacBook-Pro.local>
2018-04-11 23:36:53 +0800

    访问者模式

---
+++ kardel-web/src/main/java/mode/chapter28/Action.java
@@ -0,0 +1,11 @@
+package mode.chapter28;
+
+/**
+ * Created by dennis on 2018/4/11.
+ */
+public abstract class Action {
+
+    abstract void getManConclusion(Man man);
+
+    abstract void getWomenConclusion(Women women);
+}
---
+++ kardel-web/src/main/java/mode/chapter28/Person.java
@@ -0,0 +1,9 @@
+package mode.chapter28;
+
+/**
+ * Created by dennis on 2018/4/11.
+ */
+public abstract class Person {
+
+    abstract void accept(Action action);
+}
